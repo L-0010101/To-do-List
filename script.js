5ac28647113b07d353f0307366bde28928d8f7e1
l-0010101 <leomeloteixeira9@gmail.com>
2026-02-04 01:30:34 -0300

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+function adicionarTarefa(){
+    let valorDoInput = document.querySelector("input").value
+    let li = document.createElement("li")
+    li.innerHTML = valorDoInput + '<span onclick = "deletarTarefa(this)">❌</span>'
+    
+    document.querySelector("ul").appendChild(li)
+
+    document.querySelector("input").value = ""
+}
+function deletarTarefa(li){
+    let botaoDeletar = document.querySelector("span")
+    li.parentElement.remove()
+}
